a4b611624a2c605a3f403bb5d41df1874c399297
xphong <xphong@gmail.com>
2018-10-24 18:53:21 -0400

    fix: single package

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   title: "Taplytics Styleguide",
-  components: "packages/**/src/!(*.story).*sx"
-  // resolver: require("react-docgen").resolver.findAllComponentDefinitions,
-  // propsParser: (filePath, source, resolver, handlers) => {
-  //   return filePath.indexOf(".tsx") > -1
-  //     ? require("react-docgen-typescript")
-  //         .withDefaultConfig()
-  //         .parse(filePath, source, resolver, handlers)
-  //     : require("react-docgen").parse(source, resolver, handlers);
-  // }
+  components: "packages/**/src/**/!(*.story).*sx"
 };
